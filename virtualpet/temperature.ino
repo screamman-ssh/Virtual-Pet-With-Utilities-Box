827cb41ec30543620e865ee1793004cedb23f47b
Onpreeya Suwanchatri <6410110564@psu.ac.th>
2023-09-19 02:03:22 +0700

    add color temp

--- virtualpet/temperature.ino
+++ virtualpet/temperature.ino
@@ -1,6 +1,9 @@
+uint32_t temp_color_array[] = {0xffbf5f00, 0xffbf0000, 0xffbf005f, 0xff7f003f, 0xff007fff, 0xff0000ff};
+uint8_t temp_color = 0;
 
 void display_temp(Graphic16x16 graphic){
-  graphic.setBackground((uint32_t) 0x00000000);
+  temp_color = temp_color_selector(9);
+  graphic.setBackground(temp_color_array[temp_color]);
   graphic.drawWithColor(number3x5_data[3],0xffeaeaea,5,3,2,2);
   graphic.drawWithColor(number3x5_data[2],0xffeaeaea,5,3,6,2);
   if ((millis()-lasttime )> 1000){
@@ -12,4 +15,21 @@ void display_temp(Graphic16x16 graphic){
   graphic.draw(temp_data[0], 6, 16, 0, 8);
   delay(50);
   graphic.display();
+}
+
+uint8_t temp_color_selector(int temp){
+  uint8_t tmp;
+  if (temp > -20 && temp <= 10)
+    tmp = 0;
+  else if (temp > 10 && temp <= 20)
+    tmp = 1;
+  else if (temp > 20 && temp <= 29)
+    tmp = 2;
+  else if (temp > 29 && temp <= 35)
+    tmp = 3;
+  else if (temp > 35 && temp <= 40)
+    tmp = 4;
+  else
+    tmp = 5;
+  return tmp;
 }
